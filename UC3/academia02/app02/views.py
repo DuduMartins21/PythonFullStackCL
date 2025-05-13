876f3eff7fb628d7bd25b3970ea39a72ed9adfea
DuduMartins21 <cdudusm@gmail.com>
2025-05-12 21:35:04 -0300

    Add files via upload

---
+++ UC3/academia02/app02/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("<h1>Site da Academia - APP 02</h1>")
+
+def franquia(request):
+    return HttpResponse("<h1>Site da Franquia - APP 02</h1>")
+
+def faleconosco(request):
+    return HttpResponse("<h1>Site da Fale conosco - APP 02</h1>")
